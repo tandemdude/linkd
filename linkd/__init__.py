6d63db1df4f46b79df1afa513175fac21e21e317
tandemdude <43570299+tandemdude@users.noreply.github.com>
2025-04-08 18:37:25 +0100

    chore: update description

--- linkd/__init__.py
+++ linkd/__init__.py
@@ -18,7 +18,7 @@
 # LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
-"""A powerful async dependency injection framework for Python."""
+"""A powerful async-only dependency injection framework for Python."""
 
 from linkd.conditions import *
 from linkd.container import *
