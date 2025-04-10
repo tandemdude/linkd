d1bd535a67fb2ce332aaa7a018801f6c4e5915e3
tandemdude <43570299+tandemdude@users.noreply.github.com>
2025-04-10 10:46:03 +0100

    docs: add missing docstrings

--- linkd/context.py
+++ linkd/context.py
@@ -38,26 +38,82 @@ DefaultContainer = t.NewType("DefaultContainer", container.Container)
 
 
 class ContextRegistry:
+    """
+    A registry for all DI contexts known to linkd. This acts as a mapping of a context to the
+    type that the container will register itself as when one is created for that context, allowing
+    accessing a specific context's container from anywhere within a nested DI flow.
+
+    It is not absolutely necessary to register a context to a registry in order to use it, however
+    it is required if you want to provide a custom injectable type for that container automatically.
+    """
+
     __slots__ = ("_contexts",)
 
     def __init__(self) -> None:
         self._contexts: dict[Context, type[container.Container] | None] = {}
 
     def register(self, ns: str, linked_type: type[container.Container] | None = None) -> Context:
+        """
+        Create and register a DI context to this registry, along with an optional injectable type.
+
+        Args:
+            ns: The namespace for the new context. This should be an identifier unique to your library/application.
+                For example, linkd uses the root namespace ``linkd.contexts.<...extras>`` for its included contexts.
+            linked_type: The injectable container type for this context. Defaults to :obj:`None`.
+
+        Returns:
+            The created context.
+
+        Raises:
+            :obj:`KeyError`: If a context using the same identifier already exists.
+
+        Example:
+
+            .. code-block:: python
+
+                class YourContexts:
+                    DEFAULT = linkd.Contexts.DEFAULT
+                    FOO = linkd.global_context_registry.register("com.example.contexts.foo")
+        """
+        if ns in self._contexts:
+            raise KeyError("A context with name '{}' already exists".format(ns))
+
         self._contexts[c := Context(ns)] = linked_type
         return c
 
     def unregister(self, context: Context) -> None:
+        """
+        Unregister a DI context from this registry.
+
+        Args:
+            context: The context to unregister.
+
+        Returns:
+            :obj:`None`
+
+        Raises:
+            :obj:`KeyError`: If the context had not previously been registered.
+        """
         del self._contexts[context]
 
     def type_for(self, context: Context) -> type[container.Container] | None:
+        """
+        Get the stored type for the given context. This function is used internally when containers are created.
+
+        Args:
+            context: The context to get the type for.
+
+        Returns:
+            The stored type for the given context, or :obj:`None` if no type was stored, or the
+            context was not registered.
+        """
         if context in self._contexts:
             return self._contexts[context]
-        # TODO - maybe error that context has not been registered?
         return None
 
 
 global_context_registry: ContextRegistry = ContextRegistry()
+"""The global context registry."""
 
 
 @t.final
